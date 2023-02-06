2f2cd733d744d15b10a557f05716834f8c9d3594
kmode <kmode@DESKTOP-N8NVV7V>
2023-02-06 16:30:55 +0100

    List Inheritance

--- src/main/scala/ExampleApps/TimeApp/TimeApp.scala
+++ src/main/scala/ExampleApps/TimeApp/TimeApp.scala
@@ -1,9 +1,5 @@
 package ExampleApps.TimeApp
 
-/*
-@author kmodelewski.com
-*/
-
 object TimeApp extends App {
 
   println("Hello")
--- src/main/scala/ExampleApps/TimeApp/TimePrinter.scala
+++ src/main/scala/ExampleApps/TimeApp/TimePrinter.scala
@@ -1,8 +1,5 @@
 package ExampleApps.TimeApp
 
-import java.time.format.DateTimeFormatter
-import java.time.{ZoneId, ZonedDateTime}
-
 
 /*
 @author kmodelewski.com
